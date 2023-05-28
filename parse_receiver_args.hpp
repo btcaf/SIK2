936b14eda129fc4572dffb956526615712e57b88
cbott <c.botta2003@gmail.com>
2023-05-29 01:00:15 +0200

    poprawny plik

---
+++ parse_receiver_args.hpp
@@ -0,0 +1,8 @@
+#ifndef SIKRADIO_PARSE_RECEIVER_ARGS_HPP
+#define SIKRADIO_PARSE_RECEIVER_ARGS_HPP
+
+#include "temp_receiver.hpp"
+
+Temp_Receiver parse_receiver_args(int argc, char *argv[]);
+
+#endif //SIKRADIO_PARSE_RECEIVER_ARGS_HPP
